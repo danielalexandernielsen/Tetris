60ffcf7ebda02261b249f1aabf78908f023429b8
danielalexandernielsen <56228796+danielalexandernielsen@users.noreply.github.com>
2019-12-11 00:11:50 +0100

    v013

--- Tetris/Tetris.cs
+++ Tetris/Tetris.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Media;
 
 namespace Tetris
 {
@@ -11,16 +12,22 @@ namespace Tetris
             Console.Title = "Tetris";
             Console.CursorVisible = false;
 
+            using (SoundPlayer player = new SoundPlayer("Shantris.wav"))
+            {
+                player.PlayLooping();
+            }
+
             int interfaceWidth = 40;
             double windowRatio = 1;
             string aspectRatio = new string(' ', 2);
             int[] windowSizeXY = Window.SetWindow(windowRatio, interfaceWidth);
 
+
             while (true)
             {
                 List<Tuple<string, int, int>> tetromino = Generate.NewTetromino();
                 var linesToClear = Draw.Game(windowSizeXY[0], windowSizeXY[1], aspectRatio, tetromino);
-                Score.ClearLine(linesToClear);
+                // Score.ClearLine(linesToClear);
                 Score.SetScore();
                 Thread.Sleep(150);
             }
